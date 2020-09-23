086968694212bfeb4578a285bbfec691d9fda0d5
talantimofei1994 <talantimofei1994@gmail.com>
2020-09-23 16:59:49 +0300

    44
    Test

---
+++ js/main.js
@@ -0,0 +1,59 @@
+(function() {
+	const header = document.querySelector('.header');
+	window.onscroll = () => {
+		if (window.pageYOffset > 50) {
+			header.classList.add('header__active');
+		}
+		else{
+			header.classList.remove('header__active');
+		}
+    };
+    function scrollTo(element){
+        window.scroll({
+            behavior: 'smoooth',
+            left: 0,
+            top: element.offsetTop
+        });
+    }
+document.getElementById("button").addEventListener('click',()=>{
+    scrollTo(document.getElementById("8"));
+});
+   /**
+(function () {
+
+    const smoothScroll = function (targetEl, duration) {
+        const headerElHeight =  document.querySelector('.menu').clientHeight;
+        let target = document.querySelector(targetEl);
+        let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+        let startPosition = window.pageYOffset;
+        let startTime = null;
+    
+        const ease = function(t,b,c,d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        };
+    
+        const animation = function(currentTime){
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, targetPosition, duration);
+            window.scrollTo(0,run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        };
+        requestAnimationFrame(animation);
+
+    };
+
+    const scrollTo = function () {
+        const links = document.querySelectorAll('.js-scroll');
+        links.forEach(each => {
+            each.addEventListener('click', function () {
+                const currentTarget = this.getAttribute('href');
+                smoothScroll(currentTarget, 1000);
+            });
+        });
+    };
+    scrollTo();
+}()); */
